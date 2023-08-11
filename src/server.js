7aed937229acee1882c30829a005d5bc03a5acb9
Gsnail157 <GaryWG.0120@gmail.com>
2023-08-10 21:53:52 -0400

    init

--- src/server.js
+++ src/server.js
@@ -2,13 +2,16 @@ let express = require("express");
 let { Pool } = require("pg");
 //let env = require("../env.json");
 
+// Use when address already in use error
+//https://stackoverflow.com/questions/4075287/node-express-eaddrinuse-address-already-in-use-kill-server
+
 const hostname = "localhost";
-const port = 3000;
+const port = 8080;
 
 let app = express();
 // let pool = new Pool(env);
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 // pool.connect().then(() => {
 //     console.log("Connected to Database");
 // });
